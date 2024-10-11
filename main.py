8a38210b222cc3d72eac8e6aff221048f4063c9c
SAHFEERUL WASIHF <97935816+SAHFEERULWASIHF@users.noreply.github.com>
2024-10-11 17:02:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from face_recognition.face_recognition import FaceRecognition
 
 if __name__ == "__main__":
-    video_path = 'input/spiderman.mp4'
+    video_path = 'input/spiderman.mp4' #replace with your video file name
     face_recognition = FaceRecognition(video_path)
     face_recognition.run()
